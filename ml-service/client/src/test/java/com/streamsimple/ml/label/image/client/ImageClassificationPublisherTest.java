c4b9c187a70fdf6b992712a1b47673fee07c691b
Timothy Farkas <tim@simplifi.it>
2017-12-02 19:39:00 -0800

    - Added ImageClassificationPublisher unit test.

--- ml-service/client/src/test/java/com/streamsimple/ml/label/image/client/ImageClassificationPublisherTest.java
+++ ml-service/client/src/test/java/com/streamsimple/ml/label/image/client/ImageClassificationPublisherTest.java
@@ -1,4 +1,4 @@
-package com.streamsimple.sdk.client.pubsub;
+package com.streamsimple.ml.label.image.client;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -9,44 +9,35 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import com.google.common.collect.Lists;
-import com.simplifi.it.javautil.serde.StringDeserializer;
-import com.simplifi.it.javautil.serde.StringSerializer;
 import com.streamsimple.kafka.testutils.KafkaClusterTestWatcher;
-import java.nio.charset.StandardCharsets;
+import com.streamsimple.sdk.client.pubsub.KafkaProtocol;
+import com.streamsimple.sdk.ml.data.ImageClassificationRequestOuterClass.ImageClassificationRequest;
 import java.util.Iterator;
 import java.util.Properties;
 
-public class KafkaPublisherTest
+public class ImageClassificationPublisherTest
 {
   @Rule
   public final KafkaClusterTestWatcher kafkaTestWatcher = new KafkaClusterTestWatcher.Builder().build();
 
   @Test
-  public void test() throws Exception
+  public void simplePublisherTest() throws Exception
   {
-    final String topic = "testTopic";
-
-    final StringSerializer serializer = new StringSerializer.Builder()
-        .setCharsetName(StandardCharsets.UTF_8.name())
-        .build();
-
-    final StringDeserializer deserializer = new StringDeserializer.Builder()
-        .setCharsetName(StandardCharsets.UTF_8.name())
-        .build();
+    final String topic = "imageTopic1";
 
     kafkaTestWatcher.createTopic(topic, 1);
 
-    final com.streamsimple.sdk.client.pubsub.KafkaProtocol.Publisher publisherProtocol =
-        new com.streamsimple.sdk.client.pubsub.KafkaProtocol.Publisher.Builder()
-        .addBootstrapEndpoints(kafkaTestWatcher.getBootstrapEndpoints())
-        .setTopic(topic)
-        .build();
+    final KafkaProtocol.Publisher publisherProtocol =
+        new KafkaProtocol.Publisher.Builder()
+            .addBootstrapEndpoints(kafkaTestWatcher.getBootstrapEndpoints())
+            .setTopic(topic)
+            .build();
 
-    final com.streamsimple.sdk.client.pubsub.Publisher<String> publisher = new PublisherFactoryImpl<String>()
-        .create(publisherProtocol, serializer);
+    final ImageClassificationPublisher publisher = new ImageClassificationPublisher.Builder().build(publisherProtocol);
 
-    final String expected = "My data";
-    publisher.pub(expected);
+    final byte[] expectedImage = new byte[]{1, 2, 3};
+
+    publisher.pub(expectedImage);
     publisher.close();
 
     final Properties subProps = new Properties();
@@ -73,6 +64,7 @@ public class KafkaPublisherTest
     Iterator<ConsumerRecord<byte[], byte[]>> recordIterator = records.iterator();
     ConsumerRecord<byte[], byte[]> record = recordIterator.next();
 
-    Assert.assertEquals(expected, deserializer.deserialize(record.value()));
+    ImageClassificationRequest request = ImageClassificationRequest.parseFrom(record.value());
+    Assert.assertArrayEquals(expectedImage, request.getImage().toByteArray());
   }
 }
